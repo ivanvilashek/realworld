c74e17b6a121203427a58bb5f63575f667c7acba
Ivan Vilashek <ivan.vilashek@gmail.com>
2023-11-11 17:19:03 +0200

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ if (!process.env.IS_TS_NODE) {
 }
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
 }
 bootstrap();
